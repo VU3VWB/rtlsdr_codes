7679bfb1718b370e67d265944d3b3f51042fbb5c
VU3VWB <vu3vwb@gmail.com>
2023-07-28 10:11:45 +0800

    Fix the spectrogram code

--- sdr_correlator/acquire.sh
+++ sdr_correlator/acquire.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
 device="0"
-freq="100000000"
+# freq="100000000"
+freq="97700000"
 srate="2000000"
 gain="192"
 
-./rtl2binary -d "${device}" -f "${freq}" -s "${srate}" -g "${gain}"
+./rtl2binary -d 0 -f "${freq}" -s "${srate}" -g "${gain}"
 # (./rtl2binary2 &)
