32297cd4a8bb83df50be185a935e2d7019e99956
VU3VWB <vu3vwb@gmail.com>
2023-07-30 14:29:16 +0800

    Seems to work with twin RTL-SDRs sort of injection locked. Need more investigation

--- sdr_correlator/acquire.sh
+++ sdr_correlator/acquire.sh
@@ -2,10 +2,9 @@
 
 device="0"
 # freq="100000000"
-freq="145000000"
-# srate="2000000"
+freq="217800000"
 srate="3200000"
 gain="192"
 
-./rtl2binary -d 0 -f "${freq}" -s "${srate}" -g "${gain}"
-# (./rtl2binary2 &)
+./rtl2binary -d 0 -f "${freq}" -s "${srate}" -g "${gain}" &
+./rtl2binary -d 1 -f "${freq}" -s "${srate}" -g "${gain}" &
